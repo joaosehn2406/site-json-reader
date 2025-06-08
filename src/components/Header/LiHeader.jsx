1473a1c6b6a466902666ec0c1bcfbb3b0da24469
joaosehn2406 <jpssantosos5874@gmail.com>
2025-06-08 16:22:12 -0300

    feat: refactored LiHeader to use DOM React property

--- src/components/Header/LiHeader.jsx
+++ src/components/Header/LiHeader.jsx
@@ -1,19 +1,13 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 function LiHeader() {
     return (
         <nav>
             <ul className="ul-container">
-                <li>
-                    Home
-                </li>
-                <li >
-                    Funcionarios &#9662;
-                </li>
-                <li>
-                    Sobre
-                </li>
+                <li><Link to="/home" className="link-home">Home</Link></li>
+                <li><Link to="/funcionarios">Funcionários</Link></li>
+                <li><a href="#">Sobre</a></li>
             </ul>
             <div className="bt-container">
                 <button className="botao-login">Log in</button>
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/index.css'
-import Header from './components/Header/Header.jsx'
-import Home from "./pages/Home.jsx";
-import Footer from './components/Footer/Footer.jsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import './styles/index.css';
+
+import Header from './components/Header/Header.jsx';
+import Footer from './components/Footer/Footer.jsx';
+import Home from './pages/Home.jsx';
+import Funcionarios from './pages/Funcionarios.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Header />
-      <Home />
-      <Footer />
-  </StrictMode>,
-)
+    <BrowserRouter>
+        <Header />
+        <Routes>
+            <Route path="/" element={<Navigate to="/home" />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/funcionarios" element={<Funcionarios />} />
+        </Routes>
+        <Footer />
+    </BrowserRouter>
+);
