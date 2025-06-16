acd132ee0f4aa16799e15d88535cae1096414e78
joaosehn2406 <jpssantosos5874@gmail.com>
2025-06-15 23:02:42 -0300

    feat: added style to the cadastro component

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -1,15 +1,24 @@
-import './Cadastro.css'
+import './Cadastro.css';
 
 function Cadastro() {
     return (
-        <main className="home-cadastro" style={{ paddingTop: '400px' }}>
+        <main className="home-cadastro">
             <div className="container-cadastro">
-                <input
-                    type="text"
-                    name="input-id"
-                    id="input-id"
-                    placeholder="Digite o id desejado"
-                />
+                <form className="form-cadastro" onSubmit={(e) => e.preventDefault()}>
+                    <label className="label-nome" htmlFor="input-id">
+                        Buscar cadastro
+                    </label>
+                    <input
+                        className="input-text"
+                        type="text"
+                        name="input-id"
+                        id="input-id"
+                        placeholder="Digite o id desejado"
+                    />
+                    <button type="submit" className="btn-consultar">
+                        Consultar
+                    </button>
+                </form>
             </div>
         </main>
     );
