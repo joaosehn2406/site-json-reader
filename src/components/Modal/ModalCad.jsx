f6b922f9b37d7b09bd2907ec7b4878d65bfa6835
João Pedro <joao.pedro@joinin.com.br>
2025-06-24 19:58:52 -0300

    feat: added alter button to the modal

--- src/components/Modal/ModalCad.jsx
+++ src/components/Modal/ModalCad.jsx
@@ -10,6 +10,7 @@ function Modal({ isOpen, closeModal, deleteInfo, children }) {
             <div className="modal">
                 {children}
                 <button className="bt-fechar" onClick={closeModal}>Fechar</button>
+                <button className="bt-alterar">Alterar</button>
             </div>
         </div>
     );
